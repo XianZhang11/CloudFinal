e385c075571d9a2a189af06cf36665f60a63402c
XianZhang11 <xzhang35@kent.edu>
2018-05-09 15:03:55 -0400

    update email

--- shell.sh
+++ shell.sh
@@ -3,7 +3,7 @@ export FLASK_DEBUG=0
 export MAIL_SERVER=smtp.googlemail.com
 export MAIL_PORT=587
 export MAIL_USE_TLS=1
-export MAIL_USERNAME=drgdelozier@gmail.com
-export MAIL_PASSWORD=flash.22
+export MAIL_USERNAME=z372827671@gmail.com
+export MAIL_PASSWORD=xzhang35
 flask shell
 
--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ export FLASK_DEBUG=0
 export MAIL_SERVER=smtp.googlemail.com
 export MAIL_PORT=587
 export MAIL_USE_TLS=1
-export MAIL_USERNAME=drgdelozier@gmail.com
-export MAIL_PASSWORD=flash.22
+export MAIL_USERNAME=z372827671@gmail.com
+export MAIL_PASSWORD=xzhang35
 export MS_TRANSLATOR_KEY=5279b9fe290f4c84a7a4d15b926a4a45
-flask run
+flask run --host=0.0.0.0 --port=80
 
