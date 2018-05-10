097cb7d3ea9b4175947a26ad277f19b72f556070
XianZhang11 <xzhang35@kent.edu>
2018-05-09 21:10:13 -0400

    add zipcode

--- app/models.py
+++ app/models.py
@@ -21,7 +21,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
-    #zipcode = db.Column(db.String(10))
+    zipcode = db.Column(db.String(10))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     followed = db.relationship(
         'User', secondary=followers,
--- config.py
+++ config.py
@@ -13,6 +13,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['z372827671@gmail.com']
-    LANGUAGES = ['en', 'es']
+    LANGUAGES = ['en', 'es', 'zh']
     POSTS_PER_PAGE = 5
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
